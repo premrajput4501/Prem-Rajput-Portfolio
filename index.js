c9d03a5fa7104eef4e27805fc9baa30834179bac
Prem Rajput <104985726+premrajput4501@users.noreply.github.com>
2024-09-07 09:57:28 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -71,8 +71,6 @@ document.querySelectorAll(".feature.col")[8].addEventListener("mouseover",functi
   audio.play();
 })
 
-
-
 document.querySelectorAll(".card")[0].addEventListener("mouseover",function(){
   var audio=new Audio("./Sounds/modernclick1.wav");
   audio.play();
@@ -88,13 +86,19 @@ document.querySelectorAll(".card")[2].addEventListener("mouseover",function(){
   audio.play();
 })
 
+const navbarToggler = document.querySelector(".navbar-toggler");
+const navbarCollapse = document.querySelector(".navbar-collapse");
+const navLinks = document.querySelectorAll(".nav-link");
+const svgIcon = navbarToggler.querySelector("svg");
 
-const navbarToggler = document.querySelector('.navbar-toggler');
-const svgIcon = navbarToggler.querySelector('svg');
+navLinks.forEach((link) => {
+  link.addEventListener("click", function () {
+    navbarCollapse.classList.remove("show");
+  });
+});
 
-navbarToggler.addEventListener('click',function() {
-
-  const isExpanded = navbarToggler.getAttribute('aria-expanded') === 'true';
+navbarToggler.addEventListener("click", function () {
+  const isExpanded = navbarToggler.getAttribute("aria-expanded") === "true";
 
   if (isExpanded) {
     svgIcon.innerHTML = `
@@ -103,7 +107,7 @@ navbarToggler.addEventListener('click',function() {
     <path fill-rule="evenodd" d="M1.646 2.646a.5.5 0 0 1 .708 0L8 8.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708"/>
   </svg>
     `;
-  } else { 
+  } else {
     svgIcon.innerHTML = `
     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-double-up" viewBox="0 0 16 16">
     <path fill-rule="evenodd" d="M7.646 2.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 3.707 2.354 9.354a.5.5 0 1 1-.708-.708z"/>
@@ -113,5 +117,10 @@ navbarToggler.addEventListener('click',function() {
   }
 });
 
-navbarToggler.addEventListener('click', toggleIcon);
+document.querySelectorAll(".dropdown-menu a").forEach((element) => {
+  element.addEventListener("mousedown", (e) => {
+    e.stopPropagation();
+  });
+});
+
 
